63f297622226793e9e588a248276a2b1b09abeee
itofumito <ito.fumito@jp.fujitsu.com>
2015-03-25 01:20:25 +0900

    Refactor: bodyasobservable

--- K4W2Rx/Extensions/BodyFrameReaderExtension.cs
+++ K4W2Rx/Extensions/BodyFrameReaderExtension.cs
@@ -18,20 +18,21 @@ namespace K4W2Rx.Extensions
 
         public static IObservable<IEnumerable<Body>> BodyAsObservable(this BodyFrameReader reader)
         {
-            return BaseReaderExtension.AsObservable<BodyFrameArrivedEventArgs>(reader).Select(e =>
-            {
-                List<Body> bodies = new List<Body>();
-                using (BodyFrame bodyFrame = e.FrameReference.AcquireFrame())
+            return BaseReaderExtension.AsObservable<BodyFrameArrivedEventArgs>(reader)
+                .Select(frame => frame.FrameReference.AcquireFrame())
+                .Where(frame => frame != null)
+                .Select(frame =>
                 {
-                    bodyFrame.GetAndRefreshBodyData(bodies);
+                    Body[] bodies = new Body[frame.BodyCount];
+                    frame.GetAndRefreshBodyData(bodies);
+                    frame.Dispose();
                     return bodies;
-                }
-            });
+                });
         }
 
         public static IObservable<IEnumerable<Body>> TrackedBodyAsObservable(this BodyFrameReader reader)
         {
-            return reader.BodyAsObservable().Select(e => e.Where(b => b.IsTracked).Select(b => b));
+            return reader.BodyAsObservable().Select(bodies => bodies.Where(body => body.IsTracked).Select(b => b));
         }
     }
 }
