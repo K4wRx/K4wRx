68c58c1429270f1bef605775b9d2469d94bd9c9e
itofumito <ito.fumito@jp.fujitsu.com>
2015-04-07 08:21:43 +0900

    fix path to nuspec

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -8,7 +8,7 @@ Param (
  
 # update package version in nuspec file
 Write-Output "Updating version in nuspec file"
-$nuspecPath = "$srcFolder\K4wRx\K4wRx.nuspec"
+$nuspecPath = "$srcFolder\projects\k4wrx\K4wRx\K4wRx.nuspec"
 [xml]$xml = Get-Content $nuspecPath
 $xml.package.metadata.version = $projectVersion
 $xml.Save($nuspecPath)
