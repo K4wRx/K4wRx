3285aaf82c6821b61d97548dda2f13880cdd2096
itofumito <ito.fumito@jp.fujitsu.com>
2015-04-07 19:02:41 +0900

    TrackedBodyDiscreteGestureResultsAsObservable is enabled !
    CombineLatest is cause of exceptions because i dispose frames per vgb frames arriving.
    switch to Zip to sync body and vgb streams.

--- K4wRx/Extensions/KinectSensorExtension.cs
+++ K4wRx/Extensions/KinectSensorExtension.cs
@@ -182,40 +182,37 @@ namespace K4wRx.Extensions
                 });
 
             // maximum number of synchronized vgb frame event args will flow into this stream
-            var combinedStream = CombineStreams(frameStreams);
+            var zippedStreams = ZipStreams(frameStreams);
             // tracking ids that are tracked now will flow into this stream
-            var trackingIdStream = sensor.BodyFrameSource.OpenReader().TrackedBodyAsObservable().Select(bodies => bodies.Where(b => b.IsTracked).Select(b => b.TrackingId));
+            var trackingIdStream = sensor.BodyFrameSource.OpenReader().TrackedBodyAsObservable().Select(bodies => bodies.Select(b => b.TrackingId));
 
-            return trackingIdStream.DistinctUntilChanged()
-                .CombineLatest(combinedStream, (ids, frames) =>
+            return zippedStreams.Zip(trackingIdStream, (frames, ids) =>
+            {
+                var i = ids.GetEnumerator();
+                foreach (var f in frames.Where(f => f.TrackingId == 0 || !f.IsTrackingIdValid))
                 {
-                    var freeIds = ids.Where(id => {
-                        return !frames
-                            .Where(f => f.IsTrackingIdValid && f.TrackingId != 0)
-                            .Select(f => f.TrackingId).Contains(id);
-                    }).GetEnumerator();
-
-                    foreach (var f in frames.Where(f =>  f.TrackingId == 0 || !f.IsTrackingIdValid))
+                    if (i.MoveNext())
                     {
-                        if (freeIds.MoveNext())
-                        {
-                            f.VisualGestureBuilderFrameSource.TrackingId = freeIds.Current;
-                        }
-                        else
-                        {
-                            f.VisualGestureBuilderFrameSource.TrackingId = 0;
-                        }
+                        f.VisualGestureBuilderFrameSource.TrackingId = i.Current;
                     }
+                }
 
-                    return frames.Select(frame =>
+                return frames.Select(frame =>
+                {
+                    if (frame.DiscreteGestureResults != null)
                     {
                         Dictionary<Gesture, DiscreteGestureResult> dic = frame
                             .DiscreteGestureResults.ToDictionary(kv => kv.Key, kv => kv.Value);
+                        
                         frame.Dispose();
-
                         return dic;
-                    });
-                });
+                    }
+                    else
+                    {
+                        return new Dictionary<Gesture, DiscreteGestureResult>();
+                    }
+                }).ToList();
+            });
         }
         /// <summary>
         /// Dispose all readers which are listened by KinectSensorExtension.
@@ -236,83 +233,83 @@ namespace K4wRx.Extensions
             disposables.Clear();
         }
 
-        private static IObservable<IEnumerable<T>> CombineStreams<T>(IEnumerable<IObservable<T>> streams)
+        private static IObservable<IEnumerable<T>> ZipStreams<T>(IEnumerable<IObservable<T>> streams)
         {
             var s = streams.ToList();
             switch (s.Count)
             {
                 case 2:
-                    return s[0].CombineLatest(s[1], (s1, s2) =>
+                    return s[0].Zip(s[1], (s1, s2) =>
                     {
                         return new List<T> { s1, s2 };
                     });
                 case 3:
-                    return s[0].CombineLatest(s[1], s[2], (s1, s2, s3) =>
+                    return s[0].Zip(s[1], s[2], (s1, s2, s3) =>
                     {
                         return new List<T> { s1, s2, s3};
                     });
                 case 4:
-                    return s[0].CombineLatest(s[1], s[2], s[3], (s1, s2, s3, s4) =>
+                    return s[0].Zip(s[1], s[2], s[3], (s1, s2, s3, s4) =>
                     {
                         return new List<T> { s1, s2, s3, s4 };
                     });
                 case 5:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], (s1, s2, s3, s4, s5) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], (s1, s2, s3, s4, s5) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5 };
                     });
                 case 6:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], (s1, s2, s3, s4, s5, s6) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], (s1, s2, s3, s4, s5, s6) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6 };
                     });
                 case 7:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], (s1, s2, s3, s4, s5, s6, s7) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], (s1, s2, s3, s4, s5, s6, s7) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7 };
                     });
                 case 8:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], (s1, s2, s3, s4, s5, s6, s7, s8) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], (s1, s2, s3, s4, s5, s6, s7, s8) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8 };
                     });
                 case 9:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], (s1, s2, s3, s4, s5, s6, s7, s8, s9) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], (s1, s2, s3, s4, s5, s6, s7, s8, s9) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9 };
                     });
                 case 10:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10 };
                     });
                 case 11:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11 };
                     });
                 case 12:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12 };
                     });
                 case 13:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13 };
                     });
                 case 14:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], s[13], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], s[13], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14 };
                     });
                 case 15:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], s[13], s[14], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], s[13], s[14], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15 };
                     });
                 case 16:
-                    return s[0].CombineLatest(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], s[13], s[14], s[15], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16) =>
+                    return s[0].Zip(s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10], s[11], s[12], s[13], s[14], s[15], (s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16) =>
                     {
                         return new List<T> { s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16 };
                     });
