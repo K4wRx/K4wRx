8cb9e22f40ccee5e728acec238d908d2e6b52e0d
fumito_ito <weathercook+service@gmail.com>
2015-04-07 12:32:49 +0900

    fix miss
    i miss it :(

--- scripts/version.ps1
+++ scripts/version.ps1
@@ -6,7 +6,7 @@ Param (
  
 # update package version in nuspec file
 Write-Output "Updating version in nuspec file"
-$nuspecPath = "$srcFolder\projects\$projectName\K4wRx\K4wRx.nuspec"
+$nuspecPath = "$srcFolder\projects\k4wrx\K4wRx\K4wRx.nuspec"
 [xml]$xml = Get-Content $nuspecPath
 $xml.package.metadata.version = "$projectVersion"
 $xml.Save($nuspecPath)
