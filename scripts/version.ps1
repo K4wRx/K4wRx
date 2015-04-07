b95b8c392b9939cb08a4d7aecd8cf5eb5a7ae71f
fumito_ito <weathercook+service@gmail.com>
2015-04-07 12:42:51 +0900

    use env of appveyor

--- scripts/version.ps1
+++ scripts/version.ps1
@@ -1,12 +1,6 @@
-Param (
-    $srcFolder,
-    $projectName,
-    $projectVersion
-)
- 
 # update package version in nuspec file
 Write-Output "Updating version in nuspec file"
-$nuspecPath = "$srcFolder\projects\k4wrx\K4wRx\K4wRx.nuspec"
+$nuspecPath = "\projects\k4wrx\K4wRx\K4wRx.nuspec"
 [xml]$xml = Get-Content $nuspecPath
-$xml.package.metadata.version = "$projectVersion"
+$xml.package.metadata.version = $env:APPVEYOR_BUILD_VERSION
 $xml.Save($nuspecPath)
