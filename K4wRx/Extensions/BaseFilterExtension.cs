7ddaa07db8cea6e9f8f1cf4892c83cf3edbb05d2
fumito_ito <weathercook@gmail.com>
2015-04-28 01:39:13 +0900

    add hook point to filter or jitter stream #9

---
+++ K4wRx/Extensions/BaseFilterExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace K4wRx.Extensions
+{
+    public static class BaseFilterExtension
+    {
+        public static IObservable<T2> Filter<T1, T2>(this IObservable<T1> stream, Func<IObservable<T1>, IObservable<T2>> jitter)
+        {
+            return jitter(stream);
+        }
+    }
+}
