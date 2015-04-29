650c544c0b804616a292d675714956ef7574c59c
fumito_ito <weathercook@gmail.com>
2015-04-30 03:26:19 +0900

    add template for joint smoothing

--- K4wRx/Extensions/JointSmoothingExtension.cs
+++ K4wRx/Extensions/JointSmoothingExtension.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive.Linq;
 
 namespace K4wRx.Extensions
 {
@@ -11,19 +12,36 @@ namespace K4wRx.Extensions
     {
         public static IObservable<Joint> FilterByJointSmoothing(this IObservable<Joint> stream)
         {
-            // TODO: create it.
+            return stream.Buffer(5, 1).Select(joints => SmoothJoint(joints));
+        }
+
+        public static IObservable<Joint> FilterByJointSmoothing(this IObservable<Body> stream, JointType jointType)
+        {
+            return FilterByJointSmoothing(stream.Select(b => b.Joints[jointType]));
+        }
+
+        public static IObservable<Dictionary<JointType, Joint>> FilterByJointSmoothing(this IObservable<Body> stream)
+        {
+            return stream.Buffer(5,1).Select(bodies => SmoothJoint(bodies));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="bodies">Latest 5 body data to detect smoothing. each bodies should align by time *ascending order*</param>
+        /// <returns></returns>
+        private static Dictionary<JointType, Joint> SmoothJoint(IList<Body> bodies)
+        {
             throw new Exception("this module is not defined");
         }
 
-        public static IObservable<Joint> FilterByJointSmoothing(this IObservable<Body> stream, JointType joint)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="joints">Latest 5 joint data to detect smoothing. each joints should align by time *ascending order*</param>
+        /// <returns></returns>
+        private static Joint SmoothJoint(IList<Joint> joints)
         {
-            // TODO: create it.
-            throw new Exception("this module is not defined");
-        }
-
-        public static IObservable<Joint[]> FilterByJointSmoothing(this IObservable<Body> stream)
-        {
-            // TODO: create it.
             throw new Exception("this module is not defined");
         }
     }
